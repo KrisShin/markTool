79c8c6c542fbbc18e9021a526b2a4a4163c47ced
Kris Shin <krisshin5014@outlook.com>
2022-12-10 14:31:09 +0800

    add setData and flags to set QTableView editable.

--- interface.py
+++ interface.py
@@ -33,28 +33,46 @@ class TableModel(QAbstractTableModel):
         super(TableModel, self).__init__()
         self._data = data
 
-    def data(self, index, role):
-        if role == Qt.ItemDataRole.DisplayRole:
-            value = self._data.iloc[index.row(), index.column()]
-            return str(value)
-
     def rowCount(self, index):
         return self._data.shape[0]
 
-    def columnCount(self, index):
+    def columnCount(self, parnet=None):
         return self._data.shape[1]
 
-    def headerData(self, section, orientation, role):
-        # section is the index of the column/row.
-        if role == Qt.ItemDataRole.DisplayRole:
-            if orientation == Qt.Orientation.Horizontal:
-                return str(self._data.columns[section])
+    def data(self, index, role=Qt.ItemDataRole.DisplayRole):
+        if index.isValid():
+            if role == Qt.ItemDataRole.DisplayRole or role == Qt.ItemDataRole.EditRole:
+                value = self._data.iloc[index.row(), index.column()]
+                return str(value)
 
-            if orientation == Qt.Orientation.Vertical:
-                return str(self._data.index[section])
+    def setData(self, index, value, role):
+        if role == Qt.ItemDataRole.EditRole:
+            self._data.iloc[index.row(), index.column()] = value
+            return True
+        return False
+
+    def headerData(self, col, orientation, role):
+        if (
+            orientation == Qt.Orientation.Horizontal
+            and role == Qt.ItemDataRole.DisplayRole
+        ):
+            return self._data.columns[col]
+
+    def flags(self, index):
+        return (
+            Qt.ItemFlag.ItemIsSelectable
+            | Qt.ItemFlag.ItemIsEnabled
+            | Qt.ItemFlag.ItemIsEditable
+        )
 
     def add_rows(self, rows_df):
         self._data = pd.concat((self._data, rows_df))
+
+    def del_rows(self):
+        ...
+
+    def edit_cell(self):
+        ...
 
 
 class InterfaceMianWindow(QMainWindow):
